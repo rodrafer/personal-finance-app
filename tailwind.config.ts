726dcc8b79c20c1656187bf7a7b53b24c0ae1af8
rodrafer <rodrigoff95@gmail.com>
2025-02-12 16:26:10 -0300

    feat: add PublicSans font styles and configure Tailwind CSS to use it

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      fontFamily: {
+        sans: ['PublicSans', 'sans-serif'],
+      },
+      fontWeight: {
+        extraLight: '100',
+        normal: '400',
+        bold: '700',
+        extraBold: '800',
       },
     },
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config
